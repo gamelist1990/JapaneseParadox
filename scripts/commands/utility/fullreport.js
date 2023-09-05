df4c6159479588274b439dcfa99ce945189550c8
Koukunn_ <testbing@outlook.jp>
2023-09-05 18:03:41 +0900

    PlayerID取得これによりNAMESPOOFが効かなくてもIDで識別できます

--- scripts/commands/utility/fullreport.js
+++ scripts/commands/utility/fullreport.js
@@ -69,6 +69,7 @@ async function handleFullReport(message, args) {
         const reportBody = [
             `\n§r§4[§6Paradox§4]§r 現在オンラインのプレイヤーログを収集しました §6${member.name}§r`,
             `§r§4[§6Paradox§4]§r §6${member.name}§rのゲームモードは ${getGamemode(member)}です`,
+            `§r§4[§6Paradox§4]§r §6${member.name}§rのプレイヤーIDは【 ${member.id} 】です`,
             `§r§4[§6Paradox§4]§r §6${member.name}§rは現在X= ${member.location.x.toFixed(0)} Y= ${member.location.y.toFixed(0)} Z= ${member.location.z.toFixed(0)}にいます`,
         ];
         switch (true) {
