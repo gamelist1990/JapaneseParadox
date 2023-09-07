6f0d1d1b14195be4edc011ff473155cf4781bfa8
Koukunn_ <testbing@outlook.jp>
2023-09-07 13:25:33 +0900

    どうなるか試すだけなんでバグるかもしれないです

--- scripts/gui/moderation/uiInventory/uiInvEditorRepairItemMenu.js
+++ scripts/gui/moderation/uiInventory/uiInvEditorRepairItemMenu.js
@@ -11,11 +11,20 @@ export function uiItemEditorRepairMenu(player, targetPlayer, itemSlot) {
                 console.error("Error originated from:", sourceInfo[0]);
             }
         }
-    });
+    });//テストで入れてるだけなんでちゃんと動作するか確認してないぜ
+    const durability = item.getComponent("minecraft:durability");
+    const maxDurability = durability.maxDurability;
+    let currentDamage = durability.damage;
     async function handleUIitemEditorRepairMenu(player, targetPlayer, itemSlot) {
         const repairMenu = new ModalFormData();
         //Show the stats for the item.
         repairMenu.title("§4アイテムの修理§4");
+        repairMenu.body("修理予定のプレイヤーの名前" +
+            targetPlayer.name +
+            "\n" + "\n\n耐久性: §6" +
+            (currentDamage = maxDurability - currentDamage) +
+            "/" +
+            maxDurability);
         repairMenu.toggle("修理する", false);
         repairMenu.show(player).then((InvEditorMenuUIResult) => {
             uiItemEditorRepair(InvEditorMenuUIResult, player, targetPlayer, itemSlot);
