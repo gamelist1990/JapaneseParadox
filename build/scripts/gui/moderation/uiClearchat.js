0ced670f5b5cc0f31376474410c19b66693a5edf
Koukunn_ <testbing@outlook.jp>
2023-09-08 11:02:43 +0900

    test

--- build/scripts/gui/moderation/uiClearchat.js
+++
@@ -1,8 +0,0 @@
-import { sendMsg } from "../../util";
-import { paradoxui } from "../paradoxui";
-export function uiCLEARCHAT(player) {
-    for (let clear = 0; clear < 10; clear++)
-        sendMsg("@a", "\n".repeat(60));
-    sendMsg("@a[tag=paradoxOpped]", `§f§4[§6Paradox§4]§f ${player.name}がクリアチャットを実行した`);
-    return paradoxui(player);
-}
